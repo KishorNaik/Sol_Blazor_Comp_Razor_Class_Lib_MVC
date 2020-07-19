6f88fcb5493b425d3b895c4cef1584811d63c200
Kishor Naik <kishor.naik011.net@outlook.com>
2020-07-19 12:08:21 +0530

    Code Commit

---
+++ Sol_Demo/Sol_UI/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_UI.Models;
+
+namespace Sol_UI.Controllers
+{
+    public class DemoController : Controller
+    {
+
+        public DemoController()
+        {
+            CounterM = new CounterModel();
+        }
+
+        [BindProperty]
+        public CounterModel CounterM { get; set; }
+
+        public IActionResult Index()
+        {
+            CounterM.CurrentCount = 2;
+            return View(CounterM);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_UI/Models/CounterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_UI.Models
+{
+    public class CounterModel
+    {
+        public int CurrentCount { get; set; }
+    }
+}
